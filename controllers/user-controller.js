b722c6ace2cab95201ed102d6385070c0483fbfb
Eric Webber <ewebber283@gmail.com>
2021-07-23 18:26:32 -0500

    finished user controller routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -10,12 +10,50 @@ const userController = {
         .select('-__v')
         .then(dbUserData =>res.json(dbUserData))
         .catch(err => {
-            res.status(500).json(err)
+            console.log(err);
+            res.status(500).json(err);
         });
 
     },
     getUserById({ params }, res) {
         User.findOne({ _id: params.id })
-        
-    }
-}
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .select('-__v')
+            .then(dbUserData=>res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+    
+  createUser({ body }, res) {
+    User.create(body)
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.json(err));
+  },
+
+  
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.json(err));
+  },
+
+  
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.json(err));
+  }
+};
+
+module.exports = userController;
